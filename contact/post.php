ff921da84a5b6ef2b6569d70c245d510eec871b6
Yifan Yao <fanfannnmn@gmail.com>
2017-10-09 01:17:58 -0400

    Add new feature;

---
+++ contact/post.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$current_date = date('d/m/Y H:i:s CST');
+$content = "Date: ".$current_date."\r\nName: ".$name."\r\nEmail: ".$email."\r\nMessage: "."\r\n".$message."\r\n--------------------"."\r\n";
+$compile_dir = "./message.txt";
+$file = fopen($compile_dir, "a+");
+fwrite($file, $content);
+header("Location: ./");
+exit;
+fclose($file);
---
+++ js/site.js
@@ -0,0 +1,3 @@
+function emailAlert() {
+    alert("Your email send successfully.");
+}
